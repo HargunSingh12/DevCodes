30139a2ce6763210e7f8dc059230d62a77f66e96
Hargun <hargun199920@gmail.com>
2025-07-21 01:37:18 +0530

    Minor Fixes

--- src/app/(root)/_components/LanguageSelector.tsx
+++ src/app/(root)/_components/LanguageSelector.tsx
@@ -1,4 +1,12 @@
+"use client"
+import { useCodeEditorStore } from "@/store/useCodeEditorStore";
+import { useRef, useState } from "react"
+
 function LanguageSelector({hasAccess}:{hasAcess:boolean}) {
+
+  const {isOpen ,setIsOpen} = useState(false);
+  const {language, setLanguage} = useCodeEditorStore();
+  const dropdownRef = useRef<HTMLDivElement>(null);
   return (
     <div>
       LanguageSelect
